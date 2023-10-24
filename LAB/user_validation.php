f86e2f8e72422b7735e6b6d233bddf8982be7d28
Matthew N <metiunikolaus@gmail.com>
2023-10-24 17:59:30 +0700

    Update user_validation.php

--- LAB/user_validation.php
+++ LAB/user_validation.php
@@ -3,12 +3,4 @@ if (!isset($_COOKIE['user_id'])){
     echo "<script>window.location = 'index.php?error';</script>";
     exit();
 }
-
-/* if (!isset($_COOKIE['user_id'])){
-    header("Location: index.php?error");
-    exit();
-}else{
-    echo "tes";
-    unset($_SESSION['error']);
-}*/
 ?>
